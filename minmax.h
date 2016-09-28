879ab51a9defe5cf93365fe4b9d50e39aa01fd6a
daojyun <daojyun@gmail.com>
2016-09-28 18:33:49 +0800

    try boom

--- minmax.h
+++ minmax.h
@@ -6,7 +6,9 @@ void minmax(GAME state,int depth)
 	
 	// next move
 
-	for()
-		minmax(nexr_state,depth-1);
+	for() // the other
+		minmax(next_...,depth-1);
+	for() // self
+		minmax(next_state,depth-1);
 
 }
